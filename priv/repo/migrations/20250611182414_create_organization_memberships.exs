edec1c95dba7fb194f5583699cb1f3c3c60afe24
Dhony Silva <dhony.web@gmail.com>
2025-06-11 15:30:33 -0300

    Update 20250611182414_create_organization_memberships.exs

--- priv/repo/migrations/20250611182414_create_organization_memberships.exs
+++ priv/repo/migrations/20250611182414_create_organization_memberships.exs
@@ -1,6 +1,8 @@
 defmodule Bywater.Repo.Migrations.CreateOrganizationMemberships do
   use Ecto.Migration
 
+  # mix phx.gen.schema Accounts.OrganizationMembership organization_memberships user_id:references:users organization_id:references:organizations role:string
+
   def change do
     create table(:organization_memberships) do
       add :role, :string, null: false, default: "member"
